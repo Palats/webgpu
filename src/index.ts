d2465282f08770012b86cae8313c0e423103e4ae
Pierre Palatin <pierre@palatin.fr>
2021-12-08 20:52:03 +0100

    A game of life

--- src/index.ts
+++ src/index.ts
@@ -118,7 +118,93 @@ const test2Demo = {
     `,
 }
 
-const currentDemo = test2Demo;
+// A basic game of life.
+const conwayDemo = {
+    fps: 4,
+    sizeX: 320,
+    sizeY: 200,
+    init: (data: ArrayBuffer) => {
+        const sizeX = 320;
+        const sizeY = 200;
+        const a = new Uint8Array(data);
+        for (let y = 0; y < sizeY; y++) {
+            for (let x = 0; x < sizeX; x++) {
+                const hasLife = Math.random() > 0.8;
+                const v = hasLife ? 255 : 0;
+                a[4 * (x + y * sizeX) + 0] = v;
+                a[4 * (x + y * sizeX) + 1] = v;
+                a[4 * (x + y * sizeX) + 2] = v;
+                a[4 * (x + y * sizeX) + 3] = 255;
+            }
+        }
+    },
+    code: `
+        [[block]] struct Uniforms {
+            sizex: u32;
+            sizey: u32;
+            elapsedMs: f32;
+        };
+        [[block]] struct Frame {
+            values: array<u32>;
+        };
+
+        [[group(0), binding(0)]] var<storage, read> uniforms : Uniforms;
+        [[group(0), binding(1)]] var<storage, read> srcFrame : Frame;
+        [[group(0), binding(2)]] var<storage, write> dstFrame : Frame;
+
+        fn isOn(x: i32, y: i32) -> i32 {
+            if (x < 0) { return 0; }
+            if (y < 0) { return 0; }
+            if (x >= i32(uniforms.sizex)) { return 0; }
+            if (y >= i32(uniforms.sizey)) { return 0; }
+            let idx = x + y * i32(uniforms.sizex);
+            let v = unpack4x8unorm(srcFrame.values[idx]);
+            if (v.r < 0.5) { return 0;}
+            return 1;
+        }
+
+        [[stage(compute), workgroup_size(8, 8)]]
+        fn main([[builtin(global_invocation_id)]] global_id : vec3<u32>) {
+
+            // Guard against out-of-bounds work group sizes
+            if (global_id.x >= uniforms.sizex || global_id.y >= uniforms.sizey) {
+                return;
+            }
+
+            let x = i32(global_id.x);
+            let y = i32(global_id.y);
+            let current = isOn(x, y);
+            let neighbors =
+                  isOn(x - 1, y - 1)
+                + isOn(x, y - 1)
+                + isOn(x + 1, y - 1)
+                + isOn(x - 1, y)
+                + isOn(x + 1, y)
+                + isOn(x - 1, y + 1)
+                + isOn(x, y + 1)
+                + isOn(x + 1, y + 1);
+
+            var s = 0.0;
+            if (current != 0 && (neighbors == 2 || neighbors == 3)) {
+                s = 1.0;
+            }
+            if (current == 0 && neighbors == 3) {
+                s = 1.0;
+            }
+
+            let idx = global_id.x + global_id.y * uniforms.sizex;
+            var v = unpack4x8unorm(srcFrame.values[idx]);
+            v.r = s;
+            v.g = s;
+            v.b = s;
+            v.a = 1.0;
+            dstFrame.values[idx] = pack4x8unorm(v);
+        }
+    `,
+}
+
+
+const currentDemo = conwayDemo;
 
 class Uniforms {
     sizeX = 320;
