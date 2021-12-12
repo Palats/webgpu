06f0a2c89c2d69fa7310e7a00a4092d314be6eb2
Pierre Palatin <pierre@palatin.fr>
2021-12-12 19:19:52 +0100

    Have some coordinate in render

--- src/index.ts
+++ src/index.ts
@@ -472,15 +472,30 @@ export class AppMain extends LitElement {
             vertex: {
                 module: this.device.createShaderModule({
                     code: `
+                        struct VSOut {
+                            [[builtin(position)]] pos: vec4<f32>;
+                            [[location(0)]] coord: vec2<f32>;
+                        };
                         [[stage(vertex)]]
-                        fn main([[builtin(vertex_index)]] VertexIndex : u32)
-                            -> [[builtin(position)]] vec4<f32> {
-                        var pos = array<vec2<f32>, 3>(
-                            vec2<f32>(0.0, 0.5),
-                            vec2<f32>(-0.5, -0.5),
-                            vec2<f32>(0.5, -0.5));
+                        fn main([[builtin(vertex_index)]] idx : u32) -> VSOut {
+                            var data = array<vec2<f32>, 6>(
+                                vec2<f32>(-0.5, -0.5),
+                                vec2<f32>(0.5, -0.5),
+                                vec2<f32>(0.5, 0.5),
 
-                        return vec4<f32>(pos[VertexIndex], 0.0, 1.0);
+                                vec2<f32>(-0.5, -0.5),
+                                vec2<f32>(-0.5, 0.5),
+                                vec2<f32>(0.5, 0.5),
+                            );
+
+                            let pos = data[idx];
+
+                            var out : VSOut;
+                            out.pos = vec4<f32>(pos, 0.0, 1.0);
+                            out.coord.x = pos.x + 0.5;
+                            out.coord.y = 0.5 - pos.y;
+
+                            return out;
                         }
                     `,
 
@@ -491,8 +506,8 @@ export class AppMain extends LitElement {
                 module: this.device.createShaderModule({
                     code: `
                         [[stage(fragment)]]
-                        fn main() -> [[location(0)]] vec4<f32> {
-                            return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+                        fn main([[location(0)]] coord: vec2<f32>) -> [[location(0)]] vec4<f32> {
+                            return vec4<f32>(coord.x, coord.y, 0.0, 1.0);
                         }
                     `,
                 }),
@@ -571,11 +586,10 @@ export class AppMain extends LitElement {
         );
 
         // Rendering.
-        const textureView = this.context.getCurrentTexture().createView();
         const renderPassDescriptor: GPURenderPassDescriptor = {
             colorAttachments: [
                 {
-                    view: textureView,
+                    view: this.context.getCurrentTexture().createView(),
                     loadValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
                     storeOp: 'store',
                 },
@@ -583,7 +597,7 @@ export class AppMain extends LitElement {
         };
         const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
         passEncoder.setPipeline(this.renderPipeline);
-        passEncoder.draw(3, 1, 0, 0);
+        passEncoder.draw(6, 1, 0, 0);
         passEncoder.endPass();
 
         // And submit the work.
